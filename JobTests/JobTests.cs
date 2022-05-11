d25c4d1f0d438ed8658049fede9833f75a3070a2
Earl Harris <earleharrisjr@gmail.com>
2022-05-11 13:54:03 -0400

    DONE!

--- JobTests/JobTests.cs
+++ JobTests/JobTests.cs
@@ -23,6 +23,29 @@ namespace JobTests
                 && job1.EmployerLocation.Value == "Desert"
                 && job1.JobType.Value == "Quality control"
                 && job1.JobCoreCompetency.Value == "Persistence");
-                    }
+        }
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+            Assert.IsFalse(job1.Id == job2.Id);
+        }
+        [TestMethod]
+        public void TestToString()
+        {
+            Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+
+            string testJobOutput = $"\nID: {job1.Id}" +
+            $"\nName: {job1.Name}\nEmployer: {job1.EmployerName.Value}" +
+            $"\nLocation: {job1.EmployerLocation.Value}" +
+            $"\nPosition Type: {job1.JobType.Value}" +
+            $"\nCore Competency: {job1.JobCoreCompetency.Value}";
+
+            Assert.IsTrue(job1.ToString() == testJobOutput);
+        }
+
     }
 }
